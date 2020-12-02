52ebf7e54262c8a2ae4334391b40b6128df0e453
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 12:14:52 +0300

    Create 25.11.2020-2.java

---
+++ 25.11.2020-2.java
@@ -0,0 +1,33 @@
+public class Program{
+
+    public static void main(String[] args) {
+
+        Person kate = new Person("Kate");
+        System.out.println(kate.getName());     // Kate
+        changePerson(kate);
+        System.out.println(kate.getName());     // Kate - изменения не произошло
+        // kate хранит ссылку на старый объект
+    }
+    static void changePerson(Person p){
+        p = new Person("Alice");    // p указывает на новый объект
+        p.setName("Ann");
+    }
+    static void changeName(Person p){
+        p.setName("Alice");
+    }
+}
+class Person{
+
+    private String name;
+
+    Person(String name){
+        this.name = name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+
+        return this.name;
+    }
+}
